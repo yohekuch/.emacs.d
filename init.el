56417d348ff0008752ed2b5de48e2c1a5d920b1e
Yohei Kuchiki <extentlambda@gmail.com>
2016-07-12 01:14:12 +0900

    my settings

--- init.el
+++ init.el
@@ -6,7 +6,7 @@
 ;; No splash screen please ... jeez
 (setq inhibit-startup-message t)
 
-;; Set path to dependencies
+;; set path to dependencies
 (setq site-lisp-dir
       (expand-file-name "site-lisp" user-emacs-directory))
 
@@ -24,15 +24,10 @@
 ;; Set up appearance early
 (require 'appearance)
 
-;; Settings for currently logged in user
-(setq user-settings-dir
-      (concat user-emacs-directory "users/" user-login-name))
-(add-to-list 'load-path user-settings-dir)
-
 ;; Add external projects to load path
 (dolist (project (directory-files site-lisp-dir t "\\w+"))
   (when (file-directory-p project)
-    (add-to-list 'load-path project)))
+	(add-to-list 'load-path project)))
 
 ;; Write backup files to own directory
 (setq backup-directory-alist
@@ -47,54 +42,48 @@
 (setq-default save-place t)
 (setq save-place-file (expand-file-name ".places" user-emacs-directory))
 
-;; Are we on a mac?
-(setq is-mac (equal system-type 'darwin))
-
 ;; Setup packages
 (require 'setup-package)
 
 ;; Install extensions if they're missing
 (defun init--install-packages ()
   (packages-install
-   '(magit
-     edn
-     inflections
-     hydra
-     paredit
-     move-text
-     gist
-     htmlize
-     visual-regexp
-     markdown-mode
-     fill-column-indicator
-     flycheck
-     flycheck-pos-tip
-     flycheck-clojure
+   '(browse-kill-ring           ;; interactively insert items from kill-ring
+     change-inner               ;; Change contents based on semantic units
+     dired-details              ;; make file details hide-able in dired
+     elisp-slime-nav            ;; Make M-. and M-, work in elisp like they do in slime
+     expand-region              ;; Increase selected region by semantic units.
+     f                          ;; Modern API for working with files and directories
+     fill-column-indicator      ;; Graphically indicate the fill column
+     find-file-in-project       ;; Find files in a project quickly, on any OS
      flx
-     f
      flx-ido
-     dired-details
-     css-eldoc
-     yasnippet
-     smartparens
-     ido-vertical-mode
-     ido-at-point
-     simple-httpd
+     flycheck                   ;; On the fly syntax checking
+     flycheck-pos-tip           ;; Flycheck errors display in tooltip
+     gist                       ;; Emacs integration for gist.github.com
      guide-key
-     nodejs-repl
-     restclient
-     highlight-escape-sequences
-     whitespace-cleanup-mode
-     elisp-slime-nav
-     dockerfile-mode
-     clojure-mode
-     clojure-mode-extra-font-locking
-     groovy-mode
-     prodigy
-     cider
-     yesql-ghosts
-     string-edit
-     )))
+     highlight-escape-sequences ;; Highlight escape sequences
+     htmlize                    ;; Convert buffer text and decorations to HTML 
+     hydra                      ;; make Emacs bindings that stick around
+     ido-at-point               ;; ido-style completion-at-point
+     ido-vertical-mode
+     magit
+     markdown-mode              ;; Major mode for Markdown-formatted text
+     move-text                  ;; Move current line or region with M-up or M-down.
+     multiple-cursors
+     paredit
+     perspective                ;; switch between named "perspectives" of the editor
+     prodigy                    ;; Manage external services from within Emacs
+     restclient                 ;; An interactive HTTP client for Emacs
+     shell-command              ;; enables tab-completion for shell-command
+     simple-httpd               ;; pure elisp HTTP server
+     smart-forward              ;; Semantic navigation
+     smartparens                ;; Automatic insertion, wrapping and paredit-like navigation with user defined pairs.
+     string-edit                ;; Avoid escape nightmares by editing string in separate buffer
+     visual-regexp
+     wgrep                      ;; Writable grep buffer and apply the changes to files
+     whitespace-cleanup-mode    ;; Intelligently call whitespace-cleanup on save
+     yasnippet)))
 
 (condition-case nil
     (init--install-packages)
@@ -105,11 +94,6 @@
 ;; Lets start with a smattering of sanity
 (require 'sane-defaults)
 
-;; Setup environment variables from the user's shell.
-(when is-mac
-  (require-package 'exec-path-from-shell)
-  (exec-path-from-shell-initialize))
-
 ;; guide-key
 (require 'guide-key)
 (setq guide-key/guide-key-sequence '("C-x r" "C-x 4" "C-x v" "C-x 8" "C-x +"))
@@ -117,19 +101,25 @@
 (setq guide-key/recursive-key-sequence-flag t)
 (setq guide-key/popup-window-position 'bottom)
 
-;; Setup extensions
+;; C-h 2 backspace
+(keyboard-translate ?\C-h ?\C-?)
+
+;; C-x ? to help
+(global-set-key "\C-x?" 'help-command)
+
+;; Setup extentions
 (eval-after-load 'ido '(require 'setup-ido))
 (eval-after-load 'org '(require 'setup-org))
 (eval-after-load 'dired '(require 'setup-dired))
-(eval-after-load 'magit '(require 'setup-magit))
-(eval-after-load 'grep '(require 'setup-rgrep))
+;;(eval-after-load 'magit '(require 'setup-magit))
+;;(eval-after-load 'grep '(require 'setup-rgrep))
 (eval-after-load 'shell '(require 'setup-shell))
-(require 'setup-hippie)
-(require 'setup-yasnippet)
-(require 'setup-perspective)
-(require 'setup-ffip)
-(require 'setup-html-mode)
-(require 'setup-paredit)
+;;(require 'setup-hippie)
+;;(require 'setup-yasnippet)
+;;(require 'setup-perspective)
+;;(require 'ffip)
+;;(require 'setup-html-mode)
+;;(require 'setup-paredit)
 
 (require 'prodigy)
 (global-set-key (kbd "C-x M-m") 'prodigy)
@@ -143,24 +133,20 @@
 (--each '(css-mode-hook
           restclient-mode-hook
           js-mode-hook
-          java-mode
           ruby-mode
-          markdown-mode
-          groovy-mode
-          scala-mode)
+          markdown-mode)
   (add-hook it 'turn-on-smartparens-mode))
 
 ;; Language specific setup files
-(eval-after-load 'js2-mode '(require 'setup-js2-mode))
-(eval-after-load 'ruby-mode '(require 'setup-ruby-mode))
-(eval-after-load 'clojure-mode '(require 'setup-clojure-mode))
-(eval-after-load 'markdown-mode '(require 'setup-markdown-mode))
+;;(eval-after-load 'js2-mode '(require 'setup-js2-mode))
+;;(eval-after-load 'ruby-mode '(require 'setup-ruby-mode))
+;;(eval-after-load 'markdown-mode '(require 'setup-markdown-mode))
 
 ;; Load stuff on demand
-(autoload 'skewer-start "setup-skewer" nil t)
-(autoload 'skewer-demo "setup-skewer" nil t)
-(autoload 'auto-complete-mode "auto-complete" nil t)
-(eval-after-load 'flycheck '(require 'setup-flycheck))
+;;(autoload 'skewer-start "setup-skewer" nil t)
+;;(autoload 'skewer-demo "setup-skewer" nil t)
+;;(autoload 'auto-complete-mode "auto-complete" nil t)
+;;(eval-after-load 'flycheck '(require 'setup-flycheck))
 
 ;; Map files to modes
 (require 'mode-mappings)
@@ -185,11 +171,10 @@
 (require 'multiple-cursors)
 (require 'delsel)
 (require 'jump-char)
-(require 'eproject)
 (require 'wgrep)
 (require 'smart-forward)
 (require 'change-inner)
-(require 'multifiles)
+;; (require 'multifiles)
 
 ;; Don't use expand-region fast keys
 (setq expand-region-fast-keys-enabled nil)
@@ -212,25 +197,26 @@
 ;; Setup key bindings
 (require 'key-bindings)
 
-;; Misc
-(require 'project-archetypes)
-(require 'my-misc)
-(when is-mac (require 'mac))
+;; default browser
+(setq browse-url-browser-function 'browse-url-generic)
+(setq browse-url-generic-program 
+      (if (file-exists-p "/opt/firefox/firefox")
+          "/opt/firefox/firefox"))
 
-;; Elisp go-to-definition with M-. and back again with M-,
-(autoload 'elisp-slime-nav-mode "elisp-slime-nav")
-(add-hook 'emacs-lisp-mode-hook (lambda () (elisp-slime-nav-mode t) (eldoc-mode 1)))
 
-;; Emacs server
-(require 'server)
-(unless (server-running-p)
-  (server-start))
+(require 'auto-complete-config)
 
-;; Run at full power please
-(put 'downcase-region 'disabled nil)
-(put 'upcase-region 'disabled nil)
-(put 'narrow-to-region 'disabled nil)
+(ac-config-default)
 
-;; Conclude init by setting up specifics for the current user
-(when (file-exists-p user-settings-dir)
-  (mapc 'load (directory-files user-settings-dir nil "^[^#].*el$")))
+;; tabで補完を完了する
+(define-key ac-completing-map "\t" 'ac-complete)
+(define-key ac-completing-map "\r" nil)
+
+;; C-n, C-pで補完メニューを選択する
+(setq ac-use-menu-map t)
+;; デフォルトで設定済み
+(define-key ac-menu-map "\C-n" 'ac-next)
+(define-key ac-menu-map "\C-p" 'ac-previous)
+
+(add-to-list 'auto-mode-alist '("\\*rc$" . sh-mode))
+
