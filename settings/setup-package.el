688edccbe4df076d09f3263ca814c37a81904459
Yohei Kuchiki <extentlambda@gmail.com>
2017-08-27 16:49:56 +0900

    Add package repository gnu, org

--- settings/setup-package.el
+++ settings/setup-package.el
@@ -4,6 +4,8 @@
 ;; Add melpa to package repos
 (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
 (add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/") t)
+(add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/") t)
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
 
 (setq package-pinned-packages '())
 
