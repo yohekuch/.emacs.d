9b5d8fa1c22a950e0395ea1a2d3161f37f3a82f4
Yohei Kuchiki <extentlambda@gmail.com>
2017-08-27 16:51:35 +0900

    Add keybindings for history

--- settings/setup-python-mode.el
+++ settings/setup-python-mode.el
@@ -1,2 +1,8 @@
 (setq python-shell-interpreter "python3")
+(pyvenv-activate "~/Dropbox/hacks/pyscrape/scraping/")
+
+;; Traditional terminal console like keybindings
+(define-key inferior-python-mode-map (kbd "C-p") 'comint-previous-input)
+(define-key inferior-python-mode-map (kbd "C-n") 'comint-next-input)
+
 (provide 'setup-python-mode)
